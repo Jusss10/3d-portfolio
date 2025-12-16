29f8863652865a6a3d98017cc2dc4659da6b829a
Justine Dor <118171057+Jusss10@users.noreply.github.com>
2025-12-17 00:04:21 +0100

    Added xButton

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -7,8 +7,6 @@ import {EffectComposer, Outline } from '@react-three/postprocessing'
 import { cameraPresets } from '../types/CameraPreset'
 import CameraController from './CameraController'
 
-
-
 //global scene behaviour
 export default function Room() {
 
@@ -21,36 +19,56 @@ export default function Room() {
   }, [activePreset])
 
   return (
-    <div className="w-full h-screen">
-      <Canvas
-        camera={{
-          position: [
-            cameraPresets.default.position.x,
-            cameraPresets.default.position.y,
-            cameraPresets.default.position.z
-          ],
-          fov: 60
-        }}
-        gl={{ 
-          antialias: false,
-          toneMapping: THREE.NoToneMapping
-        }}>
-        <CameraController preset={cameraPresets[activePreset]} />
+		<div className="w-full h-screen">
+			<Canvas
+				camera={{
+					position: [
+						cameraPresets.default.position.x,
+						cameraPresets.default.position.y,
+						cameraPresets.default.position.z,
+					],
+					fov: 60,
+				}}
+				gl={{
+					antialias: false,
+					toneMapping: THREE.NoToneMapping,
+				}}>
+				<CameraController preset={cameraPresets[activePreset]} />
 
-        <Suspense fallback={null}>
-          <ambientLight intensity={0.5} color="#f4eedb"/>
-          <RoomModel 
-            setHovered={setHovered}
-            setCameraPreset={setActivePreset}  />
-        </Suspense>
+				<Suspense fallback={null}>
+					<ambientLight intensity={0.5} color="#f4eedb" />
+					<RoomModel
+						setHovered={setHovered}
+						setCameraPreset={setActivePreset}
+					/>
+				</Suspense>
 
-        <EffectComposer autoClear={false}>
-          <Outline
-            selection={hovered ? [hovered] : []}
-            edgeStrength={3}/>
-        </EffectComposer>
-      </Canvas>
-    </div>
-  )
+				<EffectComposer autoClear={false}>
+					<Outline selection={hovered ? [hovered] : []} edgeStrength={3} />
+				</EffectComposer>
+			</Canvas>
+			{activePreset !== "default" && (
+				<button
+					onClick={() => setActivePreset("default")}
+					style={{
+						position: "absolute",
+						top: 20,
+						right: 20,
+						width: 40,
+						height: 40,
+						background: "transparent",
+						border: "none",
+						cursor: "pointer",
+						zIndex: 10,
+					}}>
+					<img
+						src="/images/xButton.jpg"
+						alt="Reset camera"
+						style={{ width: "100%", height: "100%" }}
+					/>
+				</button>
+			)}
+		</div>
+	);
 }
 
